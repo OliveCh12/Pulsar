2c251b3dff46bd8a7a260c875b433197c7caa1d0
Olivier <olivierchemla@gmail.com>
2019-02-06 14:06:26 +0100

    Adding Everything !

---
+++ content/projects/_index.md
@@ -0,0 +1,7 @@
+---
+title: " My Projects"
+date: 2019-02-05T17:58:17+01:00
+draft: false
+---
+
+This is content.
--- themes/pulsar/static/js/index.js
+++ themes/pulsar/static/js/index.js
@@ -1,4 +1,5 @@
 const progressBars = document.querySelectorAll(".progress-bar")
+const benchmarkButton = document.querySelector("#start-benchmark")
 
 
 let progressValues = [20, 40]
@@ -6,13 +7,39 @@ let progressValues = [20, 40]
 function setProgress() {
     for (const key in progressBars) {
         if (progressBars.hasOwnProperty(key)) {
-            const element = progressBars[key];
-            element.style.width = "60%"
+            const progressBar = progressBars[key];
+            if (progressBar.style.width === "0%") {
+                progressBar.style.width = "80%"
+                benchmarkButton.innerHTML = 'Reset Benchmark<i class="fas fa-sync-alt ml-2 spin"></i>'
+            } else {
+                progressBar.style.width = "0%"
+                benchmarkButton.innerHTML = 'Start Benchmark<i class="fas fa-play-circle ml-2"></i>'
+            }
         }
     }
 }
 
-console.log(progressValues.map( value => value * 2))
-progressValues.forEach(function(value) {
-    30 + value
-})
+// for (let index = 0; index < progressBars.length; index++) {
+//     const element = progressBars[index];
+//     console.log(element);
+// }
+
+// let bars = setInterval(() => {
+
+// }, 2000);
+
+// if (progressBars.length === 0) {
+//     clearInterval()
+// }
+
+// for (let index = 0; index < progressValues.length; index++) {
+//     const element = progressValues[index];
+//     setTimeout(() => {
+//         console.log(element)
+//     }, 2000);
+// }
+
+// console.log(progressValues.map(value => value * 2))
+// progressValues.forEach(function (value) {
+//     30 + value
+// })
